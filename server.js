3a2d14af5ae008f435733e1d32c00255b4addf6b
tunde1256 <109033697+tunde1256@users.noreply.github.com>
2025-01-05 10:34:13 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
-const knex = require('knex'); 
+const cors = require('cors'); // Import CORS
+const knex = require('knex');
 
-const dbConfig = require('./knexfile'); 
+const dbConfig = require('./knexfile');
 
-const db = knex(dbConfig.development); 
+const db = knex(dbConfig.development);
 
 // Connect to the database
-db.raw('SELECT 1+1 AS result').then(() => {
-  console.log('Connected to the database');
-}); 
+db.raw('SELECT 1+1 AS result')
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Database connection failed:', error.message);
+  });
 
 const userRoutes = require('./routes/userRoutes');
-const adminRoutes = require('./routes/adminroutes'); 
-const PharmacyRoutes = require('./routes/phamarcyRoutes')
+const adminRoutes = require('./routes/adminroutes');
+const PharmacyRoutes = require('./routes/phamarcyRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); 
-app.use('/api', userRoutes); 
+// Add CORS middleware
+app.use(cors());
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Routes
+app.use('/api', userRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/pharmacy', PharmacyRoutes); 
-
-
-
-
+app.use('/api/pharmacy', PharmacyRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
