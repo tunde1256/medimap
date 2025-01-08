03fb9eb645f2bc08c8ad20c97da6271cd72ad77d
tunde1256 <109033697+tunde1256@users.noreply.github.com>
2025-01-08 12:35:11 +0100

    Add files via upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,20 @@
 // routes/userRoutes.js
 const express = require('express');
 const userController = require('../controller/usercontroller');
+const authenticateToken = require('../middleware/authenticateToken');
 
 const router = express.Router();
 
-router.post('/users', userController.create); // Create a new user
-router.post('/login', userController.login); // Login user
-router.get('/users/:email', userController.getByEmail); // Get user by email
-router.put('/users/:userId', userController.update); // Update user information
-router.delete('/users/:userId', userController.delete); // Delete a user
-// In your routes file
+router.post('/users', userController.create); 
+router.post('/login', userController.login); 
+router.get('/users/:email', userController.getByEmail); 
+router.put('/users/:userId', userController.update); 
+router.delete('/users/:userId', userController.delete); 
+
 router.post('/request-password-reset', userController.requestPasswordReset);
-router.post('/reset-password', userController.resetPassword);
+// router.post('/reset-password', userController.resetPassword);
+router.post('/verify',userController.verifyResetCode);
+router.post('/reset-password',userController.updatePassword);
 router.post('/logout', userController.logout);
 
 module.exports = router;
